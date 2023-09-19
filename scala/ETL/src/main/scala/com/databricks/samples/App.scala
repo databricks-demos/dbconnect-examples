4ddeafcf610f581cf8c826fdc6378ed017d9497c
Robert Dillitz <robert.dillitz@databricks.com>
2023-09-19 13:35:49 +0200

    update scala example to use databricks connect

--- scala/ETL/src/main/scala/com/databricks/samples/App.scala
+++ scala/ETL/src/main/scala/com/databricks/samples/App.scala
@@ -1,21 +1,17 @@
 package com.databricks.samples
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.catalyst.dsl.expressions.StringToAttributeConversionHelper
-import org.apache.spark.sql.execution.streaming.ProcessingTimeTrigger
-import org.apache.spark.sql.functions.window
+
+import com.databricks.connect.DatabricksSession
 
 object SparkConnectSampleApplication {
-
-  def configString: String = {
-    s"sc://${System.getenv("HOST")}:443/;user_id=na;token=${System.getenv(
-        "TOKEN")};x-databricks-cluster-id=${System.getenv("CLUSTER")}"
-  }
-
   def main(args: Array[String]) {
-    val spark = SparkSession.builder().remote(configString).getOrCreate()
+
+    val spark = DatabricksSession.builder.getOrCreate()
+
+    val catalog = "main"
+    val schema = "martingrund"
 
     println("Step 1 ....")
-    val table_name_bronze = "main.martingrund.taxi_demo_bronze"
+    val table_name_bronze = s"${catalog}.${schema}.taxi_demo_bronze"
     var df = LoadData.run(spark)
     SaveData.run(df, table_name_bronze)
 
@@ -25,12 +21,12 @@ object SparkConnectSampleApplication {
     df = TransformColums.run(df)
     df = FilterInvalid.run(df)
 
-    val table_name_silver = "main.martingrund.taxi_demo_silver"
+    val table_name_silver = s"${catalog}.${schema}.taxi_demo_silver"
     SaveData.run(df, table_name_silver)
 
     println("Step 3 ...")
-    val yellow_view = "main.martingrund.taxi_demo_view_yellow"
-    val green_view = "main.martingrund.taxi_demo_view_green"
+    val yellow_view = s"${catalog}.${schema}.taxi_demo_view_yellow"
+    val green_view = s"${catalog}.${schema}.taxi_demo_view_green"
     CreateView.run(spark, table_name_silver, yellow_view, 1)
     CreateView.run(spark, table_name_silver, green_view, 2)
 
